7afca7a235ab6a47e5a83068b26f367ea7b80901
ZineddineBk09 <eino1209@gmail.com>
2023-09-11 15:46:51 +0100

    add libs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # NextJS-NFT-Marketplace
+
 A web3 dapp built using nextjs, react, typescript, yailwind and solidity.
+
+Note: we can implement a NFT marketplace in 2 ways:
+
+1. Contract based: where the contract itself is the marketplace and holds all the NFTs.
+2. Off-chain: where the NFTs owners hold their NFTs and the marketplace is just a platform to trade them. and owners can give/revoke access to their NFTs to the marketplace.
+
+### Our approach is the second one.
--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -2,6 +2,68 @@
 
 pragma solidity ^0.8.7;
 
-contract NFTMarketplace {}
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-// 
+// Errors
+error NFTMarketplace__PriceMustBeAboveZero();
+error NFTMarketplace__NotApprovedForMarketplace();
+error NFTMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+
+contract NFTMarketplace {
+    // Types
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    // State variables
+    // NFT contract address => token ID => listing
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    // Modifiers
+    modifier notListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+
+        if (listing.price > 0) {
+            revert NFTMarketplace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    // Events
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    // Functions
+    function listIem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    ) external {
+        if (price <= 0) {
+            revert NFTMarketplace__PriceMustBeAboveZero();
+        }
+
+        IERC721 nftContract = IERC721(nftAddress);
+        if (nftContract.getApproved(tokenId) != address(this)) {
+            revert NFTMarketplace__NotApprovedForMarketplace();
+        }
+
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+}
+
+// listItem: list NFTs on marketplace
+// buyItem: buy NFTs from marketplace
+// cancelItem: cancel NFTs from marketplace
+// updateListing: update NFTs price
+// withdrawProceeds: withdraw proceeds from sales
