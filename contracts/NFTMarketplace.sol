61efac6964134f8f0a233ab736d6ce866c77d801
ZineddineBk09 <eino1209@gmail.com>
2023-09-12 11:03:30 +0100

    dev

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -4,24 +4,32 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-// ---------------------- Errors ---------------------- 
+// ---------------------- Errors ----------------------
 error NFTMarketplace__PriceMustBeAboveZero();
 error NFTMarketplace__NotApprovedForMarketplace();
 error NFTMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NFTMarketplace__NotListed(address nftAddress, uint256 tokenId);
 error NFTMarketplace__NotOwner();
+error NFTMarketplace__PriceNotMet(
+    address nftAddress,
+    uint256 tokenId,
+    uint256 price
+);
 
-// ---------------------- Types ---------------------- 
+// ---------------------- Types ----------------------
 struct Listing {
     uint256 price;
     address seller;
 }
 
 contract NFTMarketplace {
-    // ---------------------- State variables ---------------------- 
+    // ---------------------- State variables ----------------------
     // NFT contract address => token ID => listing
     mapping(address => mapping(uint256 => Listing)) private s_listings;
+    // keep track of people balances: address => balance
+    mapping(address => uint256) private s_proceeds;
 
-    // ---------------------- Modifiers ---------------------- 
+    // ---------------------- Modifiers ----------------------
     modifier notListed(
         address nftAddress,
         uint256 tokenId,
@@ -47,7 +55,16 @@ contract NFTMarketplace {
         _;
     }
 
-    // ---------------------- Events ---------------------- 
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+
+        if (listing.price <= 0) {
+            revert NFTMarketplace__NotListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    // ---------------------- Events ----------------------
     event ItemListed(
         address indexed seller,
         address indexed nftAddress,
@@ -55,13 +72,21 @@ contract NFTMarketplace {
         uint256 price
     );
 
-    // ---------------------- Functions ---------------------- 
+    event ItemBought(
+        address indexed buyer,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    // ---------------------- Functions ----------------------
     function listIem(
         address nftAddress,
         uint256 tokenId,
         uint256 price
     )
         external
+        // TODO: Have this contract accept payment in subset of ERC20 tokens (Hint: use chanlink price feeds)
         notListed(nftAddress, tokenId, msg.sender)
         isOwner(nftAddress, tokenId, msg.sender)
     {
@@ -76,6 +101,38 @@ contract NFTMarketplace {
 
         s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
         emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+
+    function buyItem(
+        address nftAddress,
+        uint256 tokenId
+    ) external payable isListed(nftAddress, tokenId) {
+        Listing memory listedItem = s_listings[nftAddress][tokenId];
+        if (msg.value < listedItem.price) {
+            revert NFTMarketplace__PriceNotMet(
+                nftAddress,
+                tokenId,
+                listedItem.price
+            );
+        }
+
+        s_proceeds[listedItem.seller] += msg.value;
+
+        delete (s_listings[nftAddress][tokenId]);
+
+        // Transfer NFT to buyer
+        IERC721(nftAddress).safeTransferFrom(
+            listedItem.seller,
+            msg.sender,
+            tokenId
+        );
+
+        emit ItemBought(
+            msg.sender,
+            nftAddress,
+            tokenId,
+            listedItem.price,
+        );
     }
 }
 
