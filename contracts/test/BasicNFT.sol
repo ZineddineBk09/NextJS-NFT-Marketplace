7a9bdcb531d4bb96c3c3a437fa281a79e2d648fd
ZineddineBk09 <eino1209@gmail.com>
2023-09-22 10:57:37 +0100

    fix

--- contracts/test/BasicNFT.sol
+++ contracts/test/BasicNFT.sol
@@ -9,19 +9,25 @@ contract BasicNFT is ERC721 {
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     uint256 private s_tokenCounter;
 
-    constructor() ERC721("Hamizzy", "HAM") {
+    event DogMinted(uint256 indexed tokenId);
+
+    constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
     }
 
-    function mintNFT() public returns (uint256) {
+    function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
-        s_tokenCounter++;
-        return s_tokenCounter;
+        emit DogMinted(s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
     }
 
     function tokenURI(
-        uint256 /*tokenId*/
-    ) public pure override returns (string memory) {
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
         return TOKEN_URI;
     }
 
