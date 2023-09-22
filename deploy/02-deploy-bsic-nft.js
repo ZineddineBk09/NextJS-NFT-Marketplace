db90fe46c7ce1037e3ee8928d992c8eb64292c23
ZineddineBk09 <eino1209@gmail.com>
2023-09-22 08:47:54 +0100

    write deploy script for basic nft contract

---
+++ deploy/02-deploy-bsic-nft.js
@@ -0,0 +1,34 @@
+const { network } = require('hardhat')
+const { verify } = require('../utils/verify')
+
+const developmentChains = ['hardhat', 'localhost']
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  log(
+    '========================= Deploying Basic NFT ============================='
+  )
+
+  const args = []
+  const basicNFT = await deploy('BasicNFT', {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  })
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log('Verifying...⌛⌛')
+    await verify(basicNFT.address, args)
+  }
+  log(
+    '========================= Finish Deploying Basic NFT ============================='
+  )
+}
+
+module.exports.tags = ['all', 'basicnft']
